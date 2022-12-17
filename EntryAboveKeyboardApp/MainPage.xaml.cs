f09b191d45335b7f967c7f76949bd18ba3ff0771
samir gc <samirgc@samirs-MacBook-Pro.local>
2022-12-17 20:11:22 +0545

    Initial Push

---
+++ EntryAboveKeyboardApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace EntryAboveKeyboardApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void Entry_Focused(System.Object sender, Microsoft.Maui.Controls.FocusEventArgs e)
+    {
+        // Decete whethe in iOS platform
+        if (DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            //iphoneX and above
+            if (Height > 800)
+            {
+                if (Height - SendUI.Y - SendUI.Height < 333)
+                {
+                    SendUI.TranslateTo(0, -313, 50);
+
+                }
+            }
+            //lower screen size of iOS
+            else
+            {
+                if (Height - SendUI.Y - SendUI.Height < 258)
+                {
+                    SendUI.TranslateTo(0, -258, 50);
+                }
+            }
+        }
+    }
+
+    void Entry_Unfocused(System.Object sender, Microsoft.Maui.Controls.FocusEventArgs e)
+    {
+        if (DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            SendUI.TranslateTo(0, 0, 50);
+        }
+    }
+}
+
+
---
+++ EntryAboveKeyboardApp/MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+namespace EntryAboveKeyboardApp;
+public static class MauiProgram
+{
+    [Obsolete]
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().UseMauiCompatibility().ConfigureMauiHandlers((handlers) =>
+        {
+
+        }).ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        return builder.Build();
+    }
+}
